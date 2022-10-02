54baf03beef8541c520c7439efe58072fe33d49f
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2022-10-03 02:38:00 +1100

    websockets credentials

---
+++ backend/src/main/java/com/choicify/backend/pusher/PusherInstance.java
@@ -0,0 +1,18 @@
+package com.choicify.backend.pusher;
+
+import com.pusher.rest.Pusher;
+import org.springframework.beans.factory.annotation.Value;
+
+public class PusherInstance {
+    private static Pusher pusher;
+
+    public PusherInstance(String appId, String key, String secret) {
+        pusher = new Pusher(appId, key, secret);
+        pusher.setCluster("ap4");
+        pusher.setEncrypted(true);
+    }
+
+    public Pusher getPusher() {
+        return pusher;
+    }
+}
